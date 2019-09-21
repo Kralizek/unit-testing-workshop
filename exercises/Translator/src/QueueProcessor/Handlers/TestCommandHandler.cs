72894938d1d86c23b380d0d6d670019ba42c4e58
Renato Golia <golia.renato@gmail.com>
2019-09-21 17:21:31 +0200

    Initial commit for Translator

---
+++ exercises/Translator/src/QueueProcessor/Handlers/TestCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Nybus;
+
+namespace QueueProcessor.Handlers
+{
+    public class TestCommandHandler : ICommandHandler<TestCommand>
+    {
+        public Task HandleAsync(IDispatcher dispatcher, ICommandContext<TestCommand> incomingCommand)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
